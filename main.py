1e45781c6a4e1c14b8e11314740e066ce5eaa333
Tomy Varghese <anuatomy@gmail.com>
2017-02-13 23:28:04 -0500

    create database

--- main.py
+++ main.py
@@ -36,10 +36,16 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+class Art(db.Model):
+    title = db.StringProperty(required = True)
+    art = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
 
 class MainPage(Handler):
     def render_front(self, title="", art= "", error=""):
-        self.render("front.html",title= title, art=art, error=error)
+        arts = db.GqlQuery("Select * from Art ORDER BY created DESC")
+        self.render("front.html",title= title, art=art, error=error, arts = arts)
 
     def get(self):
         self.render_front()
@@ -49,10 +55,13 @@ class MainPage(Handler):
         art = self.request.get("art")
 
         if title and art:
-            self.write("Thanks!")
+            a = Art(title = title, art = art)
+            a.put()
+
+            self.redirect("/")
         else:
             error = "A title and some artwork are required!"
-            self.render_front(title, art, error)    
+            self.render_front(title, art, error)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage)
